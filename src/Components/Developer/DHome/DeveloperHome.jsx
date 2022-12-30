035a384f9177bc5d5782da2a8f37429d2681b0f2
Tariq Khan <khantarikact@gmail.com>
2022-12-30 20:50:01 +0530

    feed page implement more

--- src/Components/Developer/DHome/DeveloperHome.jsx
+++ src/Components/Developer/DHome/DeveloperHome.jsx
@@ -9,12 +9,20 @@ import facebook from '../../../assets/DHome-icons/social-icon/facebook-svgrepo-c
 import twitter from '../../../assets/DHome-icons/social-icon/twitter-outline-svgrepo-com.svg';
 import instagram from '../../../assets/DHome-icons/social-icon/instagram-o-svgrepo-com.svg';
 
+import OpenBook from '../../../assets/DHome-icons/open-book.svg';
+import Camera from '../../../assets/DHome-icons/camera.svg';
+
+import SendIcon from '../../../assets/DHome-icons/send-plane-2-svgrepo-com.svg';
+import Gif from '../../../assets/DHome-icons/gif-svgrepo-com.svg';
+import Gallery from '../../../assets/DHome-icons/gallery-svgrepo-com.svg';
+import SmileFace from '../../../assets/DHome-icons/happy-face-svgrepo-com.svg';
+
 const DeveloperHome = () => {
   return (
     <section className='relative'>
       <div className='flex gap-6'>
         <div className='flex flex-col'>
-          <div className='rounded-xl shadow-gray-300 shadow-lg bg-white p-3'>
+          <div className='rounded-xl shadow-gray-200 shadow-lg bg-white p-3'>
             <div className='flex flex-row items-center gap-2 '>
               <div className='w-10 h-10'>
                 <img
@@ -27,12 +35,12 @@ const DeveloperHome = () => {
                 <input
                   type='text'
                   placeholder="What's happening?"
-                  className='bg-gray-100 py-1 px-3 w-full outline-none focus:border border-cyan-700 rounded'
+                  className='bg-gray-50 py-1 px-3 w-full outline-none focus:border-2 border-gray-300 rounded'
                 />
               </div>
             </div>
             <div className='flex items-center justify-center gap-6 pt-3'>
-              <div className='flex items-center cursor-pointer'>
+              <div className='flex items-center cursor-pointer hover:bg-gray-100 active:bg-gray-200 rounded-lg p-1 '>
                 <div className='w-5 h-5 inline-block mr-2'>
                   <img
                     src={VideoIcon}
@@ -42,7 +50,7 @@ const DeveloperHome = () => {
                 </div>
                 <span className='text-sm'>Live Video</span>
               </div>
-              <div className='flex items-center cursor-pointer'>
+              <div className='flex items-center cursor-pointer hover:bg-gray-100 active:bg-gray-200 rounded-lg p-1'>
                 <div className='w-5 h-5 inline-block mr-2'>
                   <img
                     src={PhotoIcon}
@@ -52,7 +60,7 @@ const DeveloperHome = () => {
                 </div>
                 <span className='text-sm'>Photo/Video</span>
               </div>
-              <div className='flex items-center cursor-pointer'>
+              <div className='flex items-center cursor-pointer hover:bg-gray-100 active:bg-gray-200 rounded-lg p-1'>
                 <div className='w-5 h-5 inline-block mr-2'>
                   <img
                     src={SmileyIcon}
@@ -70,7 +78,8 @@ const DeveloperHome = () => {
               </div>
             </div>
           </div>
-          <div className='rounded-xl shadow-gray-300 shadow-lg mt-4 bg-white p-3'>
+
+          <div className='rounded-xl shadow-gray-200 shadow-lg mt-6 bg-white p-3'>
             <div className='flex justify-between'>
               <div className='flex gap-3'>
                 <div className='w-10 h-10'>
@@ -98,7 +107,7 @@ const DeveloperHome = () => {
               />
             </div>
             <div className='flex justify-between mt-4'>
-              <div className='flex items-center'>
+              <div className='flex items-center cursor-pointer'>
                 <div className='w-6 h-6 '>
                   <img
                     src={require('../../../assets/DHome-icons/profile.png')}
@@ -122,10 +131,46 @@ const DeveloperHome = () => {
                 <p>17 Share</p>
               </div>
             </div>
+            <div className='my-3 bg-gray-300 h-[1px]' />
+            <div className='flex justify-between items-center text-sm'>
+              <p>Like</p>
+              <p>Comments</p>
+              <p>Share</p>
+            </div>
+            <div className='my-3 bg-gray-300 h-[1px]' />
+            <div className='flex justify-between items-center'>
+              <div className='w-10 h-10'>
+                <img
+                  src={require('../../../assets/DHome-icons/profile.png')}
+                  alt=''
+                  className='w-full h-full'
+                />
+              </div>
+              <div className='flex justify-center items-center gap-1 w-3/4 bg-gray-100 rounded pr-2'>
+                <input
+                  type='text'
+                  className='bg-gray-100 py-2 text-sm px-3 w-full outline-none focus:border-2 border-gray-300 rounded'
+                  placeholder='write a comment...'
+                />
+                <div className='w-6 h-6 cursor-pointer'>
+                  <img src={Gif} alt='' className='w-full h-full' />
+                </div>
+                <div className='w-6 h-6 cursor-pointer'>
+                  <img src={Gallery} alt='' className='w-full h-full' />
+                </div>
+                <div className='w-6 h-6 cursor-pointer'>
+                  <img src={SmileFace} alt='' className='w-full h-full' />
+                </div>
+              </div>
+              <div className='w-10 h-10 p-2 bg-blue-50 rounded-md cursor-pointer active:bg-light-blue-100'>
+                <img src={SendIcon} alt='' className='w-full h-full' />
+              </div>
+            </div>
           </div>
         </div>
-        <div>
-          <div className='rounded-xl shadow-gray-300 shadow-lg bg-white w-80'>
+
+        <div className='w-80'>
+          <div className='rounded-xl shadow-gray-200 shadow-lg bg-white '>
             <div className='flex justify-between items-center p-3'>
               <p>You Might Like</p>
               <button className='text-blue-700 text-sm'>See all</button>
@@ -186,12 +231,99 @@ const DeveloperHome = () => {
                 </div>
               </div>
               <div className='flex justify-between items-center gap-4'>
-                <button className='bg-white border-2 py-1 px-4 w-1/2 rounded-lg hover:bg-gray-300'>
+                <button className='bg-white border-2 py-1 px-4 w-1/2 rounded-lg hover:bg-gray-200'>
                   Ignore
                 </button>
                 <button className='bg-blue-500 text-white border-2 border-blue-500 py-1 px-4 w-1/2 rounded-lg hover:brightness-110'>
                   Follow
                 </button>
+              </div>
+            </div>
+          </div>
+
+          <div className='rounded-xl shadow-gray-200 shadow-lg bg-white mt-6'>
+            <div className='flex justify-between items-center p-3 border-b-2'>
+              <p className='text-sm font-semibold'>Recent Event</p>
+              <div className='w-5 cursor-pointer'>
+                <img src={ThreeDot} alt='' className='w-full h-full' />
+              </div>
+            </div>
+            <div className='m-3 flex flex-col gap-4'>
+              <div className='rounded-xl bg-gray-50 p-3'>
+                <div className='flex gap-2'>
+                  <div className='w-16 h-10 bg-light-blue-50 p-1 rounded-md'>
+                    <img src={OpenBook} alt='' className='w-full h-full' />
+                  </div>
+                  <div>
+                    <h6 className='font-semibold'>Graduation Ceremony</h6>
+                    <p className='text-xs font-medium  text-gray-500'>
+                      The graduation ceremony is after sometimes called...
+                    </p>
+                  </div>
+                </div>
+                <div className='my-3 bg-gray-300 h-[1px]' />
+                <div className='flex justify-between items-center mt-4'>
+                  <div className='flex gap-2 text-xs font-semibold text-gray-500'>
+                    <p>8 seen</p>
+                  </div>
+                  <div className='flex items-center cursor-pointer'>
+                    <div className='w-6 h-6 '>
+                      <img
+                        src={require('../../../assets/DHome-icons/profile.png')}
+                        alt=''
+                        className='w-full h-full'
+                      />
+                    </div>
+                    <div className='w-6 h-6 -ml-2'>
+                      <img
+                        src={require('../../../assets/DHome-icons/profile-2.png')}
+                        alt=''
+                        className='w-full h-full'
+                      />
+                    </div>
+                    <div className='w-6 h-6 bg-gray-600 rounded-full -ml-2 text-white text-xs flex items-center justify-center'>
+                      <span>+5</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='rounded-xl bg-gray-50 p-3 mb-3'>
+                <div className='flex gap-2'>
+                  <div className='w-16 h-10 bg-red-100 p-1 rounded-md'>
+                    <img src={Camera} alt='' className='w-full h-full' />
+                  </div>
+                  <div>
+                    <h6 className='font-semibold'>Photography Ideas</h6>
+                    <p className='text-xs text-gray-500 font-medium'>
+                      Reflections, Reflections work because they can create...
+                    </p>
+                  </div>
+                </div>
+                <div className='my-3 bg-gray-300 h-[1px]' />
+                <div className='flex justify-between mt-4 items-center'>
+                  <div className='flex gap-2 text-xs font-semibold text-gray-500'>
+                    <p>11 seen</p>
+                  </div>
+                  <div className='flex items-center cursor-pointer'>
+                    <div className='w-6 h-6 '>
+                      <img
+                        src={require('../../../assets/DHome-icons/profile.png')}
+                        alt=''
+                        className='w-full h-full'
+                      />
+                    </div>
+                    <div className='w-6 h-6 -ml-2'>
+                      <img
+                        src={require('../../../assets/DHome-icons/profile-2.png')}
+                        alt=''
+                        className='w-full h-full'
+                      />
+                    </div>
+                    <div className='w-6 h-6 bg-gray-600 rounded-full -ml-2 text-white text-xs flex items-center justify-center'>
+                      <span>+8</span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/Pages/DeveloperPages/DeveloperPage.js
+++ src/Pages/DeveloperPages/DeveloperPage.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-import DHeader from "../../Components/Developer/DHeader";
-import DNav from "../../Components/Developer/DNav";
+import DHeader from '../../Components/Developer/DHeader';
+import DNav from '../../Components/Developer/DNav';
 
 const DeveloperPage = (props) => {
   return (
@@ -10,7 +10,7 @@ const DeveloperPage = (props) => {
       <DHeader />
       <DNav />
 
-      <div className="ml-auto w-[calc(100%-14rem)] h-full mt-16 overflow-hidden rounded-xl p-4 bg-gray-100">
+      <div className='ml-auto w-[calc(100%-14rem)] h-full mt-16 overflow-hidden rounded-xl p-4 bg-gray-50'>
         <Outlet />
       </div>
     </>
